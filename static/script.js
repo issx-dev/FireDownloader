2d8c2b885aedb0b13eb24e56c59d7242f3496d3c
Issa El Mokadem <ielm0509@g.educaand.es>
2025-10-26 18:45:59 +0100

    Simplify download link handling by allowing default behavior and removing unnecessary fetch logic

--- static/script.js
+++ static/script.js
@@ -57,46 +57,15 @@ window.addEventListener("load", () => {
     if (!downloadLink) return;
 
     // Add a click event listener to the download link
-    downloadLink.addEventListener("click", async function (event) {
-        // Prevent the default action of the link
-        event.preventDefault();
-        // Get the link and filename from the data attributes in the HTML (a button with the download link)
-        const link = this.getAttribute("href");
-        const filename = this.getAttribute("data-filename");
-        const unique_filename = this.getAttribute("data-uniqueFilename")
-
-        try {
-            const response = await fetch(link);
-            if (!response.ok) throw new Error("Fallo al descargar el archivo");
-
-            // Create a blob from the response
-            const blob = await response.blob();
-            // Create a temporary URL for the blob
-            const url = URL.createObjectURL(blob);
-
-            // Create an hidden <a> link element
-            const a = document.createElement("a");
-            a.style.display = "none";
-            // Set the href attribute to the blob temporary URL
-            a.href = url;
-            // Set the download attribute to the filename (to download the file with the correct name)
-            a.download = filename;
-            // Append the link to the body (to make it work in Firefox)
-            // This is necessary because Firefox requires the link to be in the DOM before it can be clicked
-            document.body.appendChild(a);
-            // Programmatically click the link to trigger the download, because the link that the user clicked is not the same as the one that was created
-            a.click();
-            // CLEAN UP
-            // Remove the temporary link from the DOM
-            URL.revokeObjectURL(url);
-            // Return to the main page after 30 seconds
-            redirectAfterDownload(unique_filename);
-        } catch (error) {
-            // If the request fails, show an error message
-            const downloadInfo = document.getElementById("download_info");
-            downloadInfo.innerHTML = `<div class="error">Error: ${error.message}</div>`;
-            return;
-        }
+    downloadLink.addEventListener("click", function (event) {
+        // Get the unique filename from the data attribute
+        const unique_filename = this.getAttribute("data-uniqueFilename");
+        
+        // Let the browser handle the download directly (don't prevent default)
+        // The download will start automatically via the href attribute
+        
+        // Return to the main page after 30 seconds
+        redirectAfterDownload(unique_filename);
     });
 });
 
